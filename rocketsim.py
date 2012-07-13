fa5aeb794f59a2492f7279c792ce522a304c81e7
Marco 'Don' Kaulea <Don42@gmail.com>
2012-07-14 01:32:25 +0200

    Added variable Burn Rate

--- rocketsim.py
+++ rocketsim.py
@@ -5,28 +5,25 @@ from scitools.std import *
 def main():
   sr = 200                #Sampling Rate
   dt = 1.0/sr
-  simTimeLimit  = 200      # Timelimit [s]
   rocket = Rocket(dt)
   t = [0]       #time in seconds
-  burnTime = -1 * (rocket.mFuel[0] / rocket.dm)
   lastBurn = 0
-  #while dt * rocket.index < 200 + burnTime:
   while rocket.height[rocket.index] > 0.0 and rocket.index * dt < 1000:
     t.append(t[rocket.index]+dt)
     rocket.step()
-    if(rocket.height[rocket.index] <= 120 and not rocket.firing):
+    if(rocket.height[rocket.index] <= 100 and not rocket.firing):
       rocket.firing = True
-    if(rocket.height[rocket.index] <= 1000 and not rocket.parachute):
+    if(rocket.height[rocket.index] <= 1500 and not rocket.parachute):
       rocket.parachute = True
-    if(rocket.height[rocket.index] <= 1):
-      rocket.dm = -7
+    if(rocket.height[rocket.index] <= 3):
+      rocket.dm = -6
       lastBurn = rocket.index
       break
   while rocket.height[rocket.index] > 0.0:
     t.append(t[rocket.index]+dt)
     rocket.step()
     if lastBurn + 60 == rocket.index:
-      rocket.dm = -8
+      rocket.dm = -7
     if lastBurn + 65 == rocket.index:
       rocket.firing = False
 
@@ -36,26 +33,27 @@ def main():
   xlabel('time')
   ylabel('height')
   title("Height")
+  axis([600, 800, 0, 1300])
   figure()
   plot(t, rocket.speed)
   xlabel('time')
   ylabel('speed')
   title("Speed")
-  axis([590, 650, -4, 0])
-  figure()
-  plot(t, rocket.acceleration)
-  xlabel('time')
-  ylabel('acceleration')
-  title("Acceleration")
-  figure()
-  plot(t, rocket.f__all, 'k')
-  hold('on')
-  plot(t, rocket.f__gravity, 'g')
-  plot(t, rocket.f__drag, 'b')
-  plot(t, rocket.f__thrust, 'r')
-  xlabel('time')
-  ylabel('Force')
-  title("Gravity")
+  axis([600, 800, -12, 8])
+  #figure()
+  #plot(t, rocket.acceleration)
+  #xlabel('time')
+  #ylabel('acceleration')
+  #title("Acceleration")
+  #figure()
+  #plot(t, rocket.f__all, 'k')
+  #hold('on')
+  #plot(t, rocket.f__gravity, 'g')
+  #plot(t, rocket.f__drag, 'b')
+  #plot(t, rocket.f__thrust, 'r')
+  #xlabel('time')
+  #ylabel('Force')
+  #title("Gravity")
 
   raw_input()
 
@@ -87,31 +85,44 @@ class Environment():
     return self.air_pressure(height)/(self.gasConst*self.temperature(height))
 
 class Rocket():
-  mRocket     = 900   #131000.0  # mass of the rocket without fuel [kg]
+  mRocket     = 900.0   #131000.0  # mass of the rocket without fuel [kg]
   mFuel       = [230] #[2169000.0] # inital mass of the fuel [kg]
-  dm          = -6    #-13360.0  # flow of mass from the rocket [kg/s]
+  dm          = -4    #-13360.0  # flow of mass from the rocket [kg/s]
   Cd          = 0.5       # Drag Coefficient
   A_front     = 3.0       # frontal surface area [m**2]
   Cd_para     = 0.5
-  A_para      = 8.0
-  specImpulse = 150  #263.0     # specific Impulse by weight [s] look at wikipedia
+  A_para      = 10.0
+  specImpulse = 200  #263.0     # specific Impulse by weight [s] look at wikipedia
   height      = [20000.0]
   speed       = [-800.0]
   acceleration= [0.0]
+  burnRate   = [0.0]
   f__all      = [0.0]
   f__drag     = [0.0]
   f__gravity  = [0.0]
   f__thrust   = [0.0]
   firing = False
   parachute = False
+
   def __init__(self, dt):
     self.env    = Environment()
     self.dt = dt
     self.index = 0
 
+  def calc_burn_rate(self, index):
+    if self.firing:
+      newBurn = -1 * ((self.burnRate[index-1] * 0.05) + (self.dm - self.burnRate[index-1]))
+      if newBurn<=0:
+        newBurn = 0
+      elif newBurn >= self.dm:
+        newBurn = self.dm
+      return newBurn
+    else:
+      return 0.0
+
   def calc_fuel(self, index):
     if self.firing:
-      newFuel = self.mFuel[index-1] + self.dm * self.dt
+      newFuel = self.mFuel[index-1] + self.burnRate[index] * self.dt
       if newFuel <= 0:
         self.firing = False
         newFuel = 0
@@ -133,7 +144,7 @@ class Rocket():
 
   def f_thrust(self, index):
     if self.firing == True:
-      return self.specImpulse * -1 * self.env.gravity(self.height[index]) * self.dm
+      return self.specImpulse * -1 * self.env.gravity(self.height[index]) * self.burnRate[index]
     else:
       return 0
 
@@ -157,6 +168,7 @@ class Rocket():
 
   def step(self):
     self.index  = self.index + 1
+    self.burnRate.append(self.calc_burn_rate(self.index))
     self.mFuel.append(self.calc_fuel(self.index))
     self.acceleration.append(self.calc_acceleration(self.index))
     self.speed.append(self.calc_speed(self.index))
